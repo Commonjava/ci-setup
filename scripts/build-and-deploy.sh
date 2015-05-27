baf41c02c500bf90b70f307b517d000c936d2454
John Casey <jdcasey@commonjava.org>
2015-05-27 16:19:16 -0500

    initial commit with example Dockerfile, basic plugins.txt, and build-and-deploy helper script.

---
+++ scripts/build-and-deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+THIS=$(cd ${0%/*} && echo $PWD/${0##*/})
+BASEDIR=`dirname ${THIS}`
+BASEDIR=`dirname ${BASEDIR}`
+IMAGE_DIR=${BASEDIR}/image
+
+JENKINS_HOME=/opt/jenkins/home
+
+docker ps -a | grep jenkins
+RET=$?
+if [ $RET == 0 ]; then
+    echo "Removing pre-exising jenkins container."
+    docker stop jenkins
+    docker rm jenkins
+fi
+
+docker pull jenkins
+docker build --tags ci-jenkins $IMAGE_DIR/Dockerfile
+RET=$?
+
+if [ $RET != 0 ]; then
+    echo "Docker build failed. Exiting."
+    exit 1
+fi
+
+docker run --name jenkins -p 8080:8080 -v $JENKINS_HOME=/var/jenkins_home ci-jenkins
+
